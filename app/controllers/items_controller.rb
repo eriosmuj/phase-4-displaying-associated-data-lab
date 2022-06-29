698ffedfa0a1af5a798445397e6000f065c2bf40
Elizabeth Rios-Mujica <elizabeth.rmujica@gmail.com>
2022-06-29 11:35:22 -0400

    commit tree 3bc06cf345a8b86d812774030ed3ab7b6ea77582

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,13 @@
 class ItemsController < ApplicationController
+
+    def index 
+        # user = User.find_by(id: params[:user_id])
+        # if user
+        #   render json: user.items
+        # else if params[:user_id]
+        #   render json: { error: "User not found"}, status: :not_found
+        # else
+           items = Item.all
+           render json: items, include: :user  
+    end
 end
